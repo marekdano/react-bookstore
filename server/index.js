1a00834682909f62753edf900f5daf75c9f4a8c7
Marek Dano <mk.dano@gmail.com>
2017-05-06 22:56:55 +0100

    Setup node server with express and middlewares.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+// use morgan for login http requests to the console 
+const morgan = require('morgan');
+
+// middleware setup
+//const server = jsonServer.create();
+const server = express();
 const router = jsonServer.router('data.json');
 const middlewares = jsonServer.defaults();
-
 server.use(middlewares);
-server.use(router);
-server.listen(5000, function () {
-  console.log('JSON Server is running');
+server.use('/', router);
+server.use(morgan('combined'));
+server.use(bodyParser.json({ type: '*/*' }));
+
+// server setup
+const port = process.env.PORT || 5000;
+
+server.listen(port, function () {
+  console.log('Server is running on port', port);
 });
