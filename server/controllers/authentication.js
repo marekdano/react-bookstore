c613976aaf2d0948b4ab53e22b006aea08241990
Marek Dano <mk.dano@gmail.com>
2017-05-11 17:58:50 +0100

    Add Authentication controller on the server with the function of signing up a user.

---
+++ server/controllers/authentication.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// See if a user with the given email exists
+	User.findOne({ email: email }, function(err, existingUser) {
+		if (err) { return next(err); }
+
+		// If a user with email does exist, return an error
+		if (existingUser) {
+			return res.status(422).send({ error: 'Email is in use' });
+		} 
+
+		// If a user with email does NOT exist, create and save user record
+    const user = new User({
+			email: email,
+			password: password
+		})
+
+		user.save(function(err) {
+			if (err) { return next(err); }
+
+			// Respond to request indicating the user was created
+			res.json({ success: user.email + " was created" });
+		});
+	});
+} 
--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = (app) => {
-	app.get('/', (req, res, next) => {
-		res.send(['phone', 'paper']);
-	})
+	app.post('/signup', Authentication.signup);
 }
