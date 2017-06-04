c3b5022a4e3875d9780023cd055b44c8ae534675
Marek Dano <mk.dano@gmail.com>
2017-06-04 18:42:49 +0100

    Register form scaffolding.

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,14 +1,36 @@
 
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
 import * as actions from '../../actions';
 
 class Register extends Component {
 	render() {
+		const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+
 		return (
-			<div>Register</div>
+			<form>
+				<fieldset className="form-group">
+					<label>Email:</label>
+					<Field name="email" component="input" type="email" className="form-control" />
+				</fieldset>
+				<fieldset className="form-group">
+					<label>Password:</label>
+					<Field name="password" component="input" type="password" className="form-control" />
+				</fieldset>
+				<fieldset className="form-group">
+					<label>Confirm Password:</label>
+					<Field name="passwordConfirm" component="input" type="password" className="form-control" />
+				</fieldset>
+				<button action="submit" className="btn btn-primary">Register</button>
+			</form>
 		);
 	}
 }
 
-export default Register; 
+Register = reduxForm({
+	form: 'register',
+	fields: ['email', 'password', 'passwordConfirm']
+})(Register);
+
+export default connect()(Register);
