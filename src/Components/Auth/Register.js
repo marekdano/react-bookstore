1174b219b01465d67280ab34da9784467f0a1b12
Marek Dano <mk.dano@gmail.com>
2017-06-06 20:47:51 +0100

    Add validation for email, password fields in the 'register' form.

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -41,6 +41,18 @@ const renderField = ({ input, label, type, meta: { touched, error, warning } }) 
 function validate(formProps) {
 	const errors = {};
 
+	if (!formProps.email) {
+		errors.email = "Please enter an email";
+	}
+
+	if (!formProps.password) {
+		errors.password = "Please enter a password";
+	}
+
+	if (!formProps.passwordConfirm) {
+		errors.passwordConfirm = "Please enter a password confirmation";
+	}
+
 	if (formProps.password !== formProps.passwordConfirm) {
 		errors.password = "Passwords must match!";
 	}
